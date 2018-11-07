ee0b01b56a3e352701f46dd95dfb3c9fb403bdaf
shubham <44846251+phonebecho1@users.noreply.github.com>
2018-11-07 23:19:29 +0530

    phonebecho website
    phonebecho.in
    
    Sell Old Mobile, Phone, Tablet, iPhone and other Smartphones. We Offer The Best Resale Value For Your Old / Used Apple, Samsung, Mi Steps - Search Device, Get Instant Quote, Choose Pickup Slot, Get Doorstep Pickup With No Hidden Cost & On Spot Cash Payment.

---
+++ index.php
@@ -0,0 +1,268 @@
+
+
+
+
+
+
+
+
+
+
+<!DOCTYPE HTML>
+<html>
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Learn &mdash; Free Website Template, Free HTML5 Template by freehtml5.co</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Free HTML5 Website Template by freehtml5.co" />
+	<meta name="keywords" content="free website templates, free html5, free template, free bootstrap, free website template, html5, css3, mobile first, responsive" />
+	<meta name="author" content="freehtml5.co" />
+
+	
+
+  	<!-- Facebook and Twitter integration -->
+	<meta property="og:title" content=""/>
+	<meta property="og:image" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:site_name" content=""/>
+	<meta property="og:description" content=""/>
+	<meta name="twitter:title" content="" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />
+
+	<link href="https://fonts.googleapis.com/css?family=Work+Sans:300,400,500,700,800" rel="stylesheet">
+	
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="css/animate.css">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="css/icomoon.css">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+
+	<!-- Magnific Popup -->
+	<link rel="stylesheet" href="css/magnific-popup.css">
+
+	<!-- Owl Carousel  -->
+	<link rel="stylesheet" href="css/owl.carousel.min.css">
+	<link rel="stylesheet" href="css/owl.theme.default.min.css">
+
+	<!-- Theme style  -->
+	<link rel="stylesheet" href="css/style.css">
+
+	<!-- Modernizr JS -->
+	<script src="js/modernizr-2.6.2.min.js"></script>
+	<script>
+function showResult(str) {
+  if (str.length==0) {
+    document.getElementById("livesearch").innerHTML="";
+    document.getElementById("livesearch").style.border="0px";
+    return;
+  }
+  if (window.XMLHttpRequest) {
+    // code for IE7+, Firefox, Chrome, Opera, Safari
+    xmlhttp=new XMLHttpRequest();
+  } else {  // code for IE6, IE5
+    xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+  }
+  xmlhttp.onreadystatechange=function() {
+    if (this.readyState==4 && this.status==200) {
+      document.getElementById("livesearch").innerHTML=this.responseText;
+      document.getElementById("livesearch").style.border="1px solid #A5ACB2";
+    }
+  }
+  xmlhttp.open("GET","livesearch.php?q="+str,true);
+  xmlhttp.send();
+}
+</script>
+<style>
+.livesearch{
+	position: relative;
+}
+
+</style>
+	</head>
+	<body>
+		
+
+	
+	<div id="page">
+
+	
+	<?php include'include/nav.php'; ?>
+
+	<header id="fh5co-header" class="fh5co-cover" role="banner" style="background-image:url(image/phones.jpg);" data-stellar-background-ratio="0.5">
+		<div class="overlay"></div>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-8 col-md-offset-2 text-center">
+					<div class="display-t">
+						<div class="display-tc animate-box" data-animate-effect="fadeIn" style="margin-top:-100px;">
+							<h1>Sell Old Mobile Phone and Tablet </h1>
+							<h2></h2>
+							<p><form>
+<input type="text" size="30" onkeyup="showResult(this.value)">
+<div class="livesearch" id="livesearch"></div>
+</form></p>
+<p style="   font-weight: bold;">OR CHOOSE CATEGORY</p>
+							<p><a class="btn btn-primary btn-lg btn-learn" href="sell_phone.php">Mobile</a> <a class="btn btn-primary btn-lg  btn-video" href="sell_tablet.php"><i class="icon-play"></i> Tablet</a></p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</header>
+
+
+
+	
+
+
+
+
+	<div id="fh5co-blog">
+		<div class="container">
+			<div class="row animate-box">
+				<div class="col-md-8 col-md-offset-2 text-center fh5co-heading">
+					<h2>How It Works ?</h2>
+					<p>Sell your old / used Mobile Phone or Tablet at amazing prices in less than a minute. We even buy devices which are missing accessories, dead or bricked at great prices to offer.</p>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-lg-4 col-md-4">
+					<div class="fh5co-blog animate-box">
+						
+						<div class="blog-text">
+							<h3><a href=""#>FIND YOUR DEVICE</a></h3>
+							<span class="posted_on"></span>
+							<span class="comment"></span>
+							<p>Logon to our website using your PC or Mobile and find the item that you wish to sell.</p>
+							
+						</div> 
+					</div>
+				</div>
+				<div class="col-lg-4 col-md-4">
+					<div class="fh5co-blog animate-box">
+						
+						<div class="blog-text">
+							<h3><a href=""#>GET INSTANT QUOTE</a></h3>
+							<span class="posted_on"></span>
+							<span class="comment"></span>
+							<p>Answer few questions on your device condition and get instant quote.</p>
+							
+						</div> 
+					</div>
+				</div>
+				<div class="col-lg-4 col-md-4">
+					<div class="fh5co-blog animate-box">
+						
+						<div class="blog-text">
+							<h3><a href=""#>PLACE PICK UP ORDER</a></h3>
+							<span class="posted_on"></span>
+							<span class="comment"></span>
+							<p>Provide your details and choose preferred pick up date time slot to place an order with us.<p>
+							
+						</div> 
+					</div>
+				</div>
+					<div class="col-lg-4 col-md-4">
+					<div class="fh5co-blog animate-box">
+						
+						<div class="blog-text">
+							<h3><a href=""#>GET PICKUP CALL</a></h3>
+							<span class="posted_on"></span>
+							<span class="comment"></span>
+							<p>Our pick up executive will call you for door step pick up of your device.</p>
+							
+						</div> 
+					</div>
+				</div>
+					<div class="col-lg-4 col-md-4">
+					<div class="fh5co-blog animate-box">
+						
+						<div class="blog-text">
+							<h3><a href=""#>HURRAY - GET CASH</a></h3>
+							<span class="posted_on"></span>
+							<span class="comment"></span>
+							<p>Get paid instantly on successful diagnosis of device and verification of your identity.</p>
+							
+						</div> 
+					</div>
+				</div>
+					<div class="col-lg-4 col-md-4">
+					<div class="fh5co-blog animate-box">
+						
+						<div class="blog-text">
+							<h3><a href=""#>WHAT'S NEXT</a></h3>
+							<span class="posted_on"></span>
+							<span class="comment"></span>
+							<p>Don't worry. Your device will go into safe hands after we evaluate it's health and remaining life.</p>
+							
+						</div> 
+					</div>
+				</div>
+				
+				
+			</div>
+		</div>
+	</div>
+
+
+
+
+	
+	
+	
+	<?php include'include/footer.php';?>
+	
+	
+	
+	</div>
+
+	<div class="gototop js-top">
+		<a href="#" class="js-gotop"><i class="icon-arrow-up"></i></a>
+	</div>
+	
+	<!-- jQuery -->
+	<script src="js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src="js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="js/jquery.waypoints.min.js"></script>
+	<!-- Stellar Parallax -->
+	<script src="js/jquery.stellar.min.js"></script>
+	<!-- Carousel -->
+	<script src="js/owl.carousel.min.js"></script>
+	<!-- countTo -->
+	<script src="js/jquery.countTo.js"></script>
+	<!-- Magnific Popup -->
+	<script src="js/jquery.magnific-popup.min.js"></script>
+	<script src="js/magnific-popup-options.js"></script>
+	<!-- Main -->
+	<script src="js/main.js"></script>
+
+	</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
